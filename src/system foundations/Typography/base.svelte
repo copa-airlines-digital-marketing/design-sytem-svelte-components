508bef020014c562c213a134889557498dee9c65
cpatinoCopaair <cesar.arturo.patino@gmail.com>
2024-07-30 07:12:15 -0500

    better now

---
+++ src/system foundations/Typography/base.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import type { TypographyVariants } from '../../lib/components/ui/typography/variants';
+	import { Root as Base } from '../../lib/components/ui/typography/index';
+
+	export let sample: undefined | string = undefined;
+
+	const values: Array<TypographyVariants['size']> = [
+		'u6',
+		'u5',
+		'u4',
+		'u3',
+		'u2',
+		'u1',
+		'b',
+		'd1',
+		'd2',
+		'd3'
+	];
+</script>
+
+<ul>
+	{#each values as val}
+		<li class="my-grid-gutter">
+			<Base size={val}>{sample || val}</Base>
+		</li>
+	{/each}
+</ul>
--- src/system foundations/Typography/root.stories.svelte
+++ src/system foundations/Typography/root.stories.svelte
@@ -1,41 +1,22 @@
 <script context="module" lang="ts">
-	import { Root as Typography } from '../../lib/components/ui/typography/index';
+	import Base from './base.svelte';
 
 	export const meta = {
 		title: 'Typography',
-		component: Typography,
+		component: Base,
 		argTypes: {
-			size: {
-				options: ['u6', 'u5', 'u4', 'u3', 'u2', 'u1', 'b', 'd1', 'd2', 'd3'],
-				control: { type: 'select' }
-			},
-			class: { control: 'text' },
-			default: { value: { type: 'string' }, table: { categoy: 'properties', subcategory: 'slots' } }
+			sample: { control: { type: 'text' } },
+			class: { control: 'text' }
 		}
 	};
 </script>
 
 <script lang="ts">
 	import { Story, Template } from '@storybook/addon-svelte-csf';
-	import { type TypographyVariants } from '../../lib/components/ui/typography/variants';
-
-	const showcase: Array<TypographyVariants['size']> = [
-		'u6',
-		'u5',
-		'u4',
-		'u3',
-		'u2',
-		'u1',
-		'b',
-		'd1',
-		'd2',
-		'd3'
-	];
 </script>
 
 <Template let:args>
-	<!--👇 'on:click' allows to forward event to addon-actions  -->
-	<Typography {...args}>{args.size}</Typography>
+	<Base {...args} />
 </Template>
 
-<Story name="U6" args={{ size: 'u6' }}></Story>
+<Story name="Scale" args={{}} />
