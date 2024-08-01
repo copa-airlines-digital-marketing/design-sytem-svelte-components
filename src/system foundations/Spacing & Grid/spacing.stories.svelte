4b2d7a009d6d3962713701348c77314e629cd7bb
cpatinoCopaair <cesar.arturo.patino@gmail.com>
2024-08-01 00:53:59 -0500

    Vertical Spacing done

---
+++ src/system foundations/Spacing & Grid/spacing.stories.svelte
@@ -0,0 +1,17 @@
+<script context="module">
+	import Spacing from './spacing.svelte';
+	export const meta = {
+		title: 'system foundations/Spacing & Grid',
+		component: Spacing
+	};
+</script>
+
+<script lang="ts">
+	import { Story, Template } from '@storybook/addon-svelte-csf';
+</script>
+
+<Template let:args>
+	<Spacing {...args} />
+</Template>
+
+<Story id="vSpacing" name="Vertical Spacing" />
---
+++ src/system foundations/Spacing & Grid/spacing.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { cn } from '$lib/utils';
+
+	const spacingClasses = 'h-minimal h-tiny h-petit h-normal h-roomy h-spacious h-big h-huge'.split(
+		' '
+	);
+</script>
+
+<ol class="flex flex-wrap items-end justify-items-center gap-grid-gutter">
+	{#each spacingClasses as spacing}
+		<li class="flex-grow text-center">
+			<div class={cn(spacing, 'bg-primary-faded')}></div>
+			{spacing.replace('h-', '')}
+		</li>
+	{/each}
+</ol>
