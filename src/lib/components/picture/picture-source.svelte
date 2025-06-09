1ab98d3de3272eac5aadeabdda39d5579520702e
César Patiño <cesar.arturo.patino@gmail.com>
2025-06-09 08:16:14 -0500

    fix: fixed media query syntax

--- src/lib/components/picture/picture-source.svelte
+++ src/lib/components/picture/picture-source.svelte
@@ -8,9 +8,9 @@
 	export let screen: $$Props['screen'] = 'md';
 
 	const getMediaQuery = (size: $$Props['screen']) => {
-		if (size === 'sm') return 'min-width:600px';
-		if (size === 'md') return 'min-width:960px';
-		return 'min-width:1367px';
+		if (size === 'sm') return '(min-width: 600px)';
+		if (size === 'md') return '(min-width: 960px)';
+		return '(min-width: 1367px)';
 	};
 
 	const media = getMediaQuery(screen);
